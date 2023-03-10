e7ca0461c4a519dea423446b0f007cf1c09e7b13
Jeselyn Tania <jeselyntan01@gmail.com>
2023-03-10 23:44:39 +0700

    OK

--- ToDo/main.js
+++ ToDo/main.js
@@ -34,11 +34,25 @@ window.addEventListener('load', () => {
 	DisplayTodos()
 })
 
-function switchTheme(){
-	const theme = 
-document.getElementById("theme").value;
-	document.getElementsByTagName("meta")
-	[0].content = theme;
+const colorSwitch = document.getElementById("input-color-switch");
+colorSwitch.addEventListener("click", checkMode);
+function checkMode() {
+  console.log("checking...");
+  if (colorSwitch.checked) {
+    console.log("dark on");
+    darkModeOn();
+  } else {
+    console.log("dark off");
+    darkModeOff();
+  }
+}
+
+function darkModeOn() {
+  document.body.classList.add("dark-mode");
+}
+
+function darkModeOff() {
+  document.body.classList.remove("dark-mode");
 }
 
 function DisplayTodos () {
